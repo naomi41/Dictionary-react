70a2f988c0349f60d2c59c040f77e896616d783a
naomi41 <naomirozenberg3@gmail.com>
2021-08-03 14:52:38 +0300

    working

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,17 @@
 import React, {useState} from "react"; 
+import axios from "axios";
 
 export default function Dictionary(){
     const [keyword, setKeyword ]= useState(null);
+
+    function handleResponse(response){
+    console.log(response.data[0])
+    }
     function search(event){
         event.preventDefault();
         alert(`searching for ${keyword}`)
+        let url= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+    axios.get(url).then(handleResponse)
     }
     function handleKeyboard(event){
         setKeyword(event.target.value);
